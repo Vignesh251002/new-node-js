7a4900926b56900026abb55a2710c804892c8cb9
Vignesh25102 <vignesh.i@meyicloud.com>
2025-12-05 17:32:18 +0530

    frouth commit

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-pm2 start server.js || pm2 restart server.js
+cd /home/ec2-user/app
+
+# Ensure correct permissions for the app folder
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app
+
+# Start app using full PM2 path
+sudo -u ec2-user /usr/bin/pm2 start server.js || sudo -u ec2-user /usr/bin/pm2 restart server.js
+
